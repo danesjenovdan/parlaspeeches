1c8219f31ec7636855eacdac68e866a1b6f7772d
Tomaž <cofek0@gmail.com>
2017-08-26 16:40:44 +0200

    Upload file in admin

--- speeches/views.py
+++ speeches/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
-# Create your views here.
+def upload_srt(request):
+    print("Sejv fajla")
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return redirect('/admin/speeches/speech/')
+    return redirect('/admin/speeches/speech/')
