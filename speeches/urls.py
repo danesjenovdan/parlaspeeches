9cbdec5ec996c61a32ae83aa6a3c8ecd7911f0fa
Tomaž <cofek0@gmail.com>
2017-08-29 11:50:08 +0200

    Auto stash before merge of "master" and "origin/master"

--- speeches/urls.py
+++ speeches/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
-from .views import getSpeech
-from .views import upload_srt
+
+from .views import upload_srt, search, getSpeech
+
 
 urlpatterns = [
     url(r'^upload_srt/$', upload_srt, name='upload_srt'),
-    url(r'^getSpeech/(?P<video_id>.+)', getSpeech)
+    url(r'^getSpeech/(?P<video_id>.+)', getSpeech),
+    url(r'^q/(?P<words>[ÖÜØÄÂÁÉÓÚÍÎöüøäâáéóúíîčćšžČĆŠŽa-zA-Z0-9 \-\+!"%\.,]+)', search),
     ]
--- speeches/views.py
+++ speeches/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
 from itertools import groupby
-import re
-from .models import *
-import datetime
+
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
-from parlaspeeches.settings import MEDIA_ROOT
-from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
 from django.http import JsonResponse
+
+from parlaspeeches.settings import MEDIA_ROOT
+from django.http import JsonResponse
+
+from .models import Speech, Person
+
+import json
+import requests
+import re
+import datetime
+
+
+SOLR_URL = 'http://localhost:8983/solr/parlaspeeches'
+
 
 def upload_srt(request):
     print("Sejv fajla")
@@ -22,73 +32,147 @@ def upload_srt(request):
     return redirect('/admin/speeches/speech/')
 
 
-
 def parser(filename, myfile):
-	spl = []
-	tab = []
-	tb = 0
-	if len(Speech.objects.filter(video_id=str(myfile))) == 0:
-		person = re.compile("^:[A-ZŽČŠĐĆ]*:")
-		with open(filename) as f:
-			res = [list(g) for b,g in groupby(f, lambda x: bool(x.strip())) if b]
-			for spe in res:
-				print (spe)
-				name_parser = (person.match(spe[2]))
-				if name_parser is not None:
-					np = name_parser.group().replace(':','')
-					if len(Person.objects.filter(name_parser=np)) == 0:
-						per = Person(name_parser=np)
-						per.save()
-					if len(tab) != 0:
-						print (person.match(tab[0]).group().replace(':',''))
-						print (np)
-						if person.match(tab[0]).group().replace(':','') != np:
-							spl = (spl[0])
-							speech = Speech(speaker=Person.objects.get(name_parser=spl['person']),
-								   			content=''.join(spl['speeches']).replace(str(':'+spl['person']+':'), ''),
-								   			start_time_stamp=spl['st'],
-								   			end_time_stamp = tb,
-								   			video_id = myfile
-								)
-							speech.save()
-							spl = []
-							tab = []
-							tab.append(spe[2])
-							spl.append({'person': np,'speeches': tab, 'st': toMS(spe[1], 0), 'et': 0})
-					else:
-						tab.append(spe[2])
-						spl.append({'person': np,'speeches': tab, 'st': toMS(spe[1], 0), 'et': 0})
-				else:
-					tab.append(spe[2])
-					tb = toMS(spe[1], 1)
+    spl = []
+    tab = []
+    tb = 0
+    if len(Speech.objects.filter(video_id=str(myfile))) == 0:
+        person = re.compile("^:[A-ZŽČŠĐĆ]*:")
+        with open(filename) as f:
+            res = [list(g) for b,g in groupby(f, lambda x: bool(x.strip())) if b]
+            for spe in res:
+                print (spe)
+                name_parser = (person.match(spe[2]))
+                if name_parser is not None:
+                    np = name_parser.group().replace(':','')
+                    if len(Person.objects.filter(name_parser=np)) == 0:
+                        per = Person(name_parser=np)
+                        per.save()
+                    if len(tab) != 0:
+                        print (person.match(tab[0]).group().replace(':',''))
+                        print (np)
+                        if person.match(tab[0]).group().replace(':','') != np:
+                            spl = (spl[0])
+                            speech = Speech(speaker=Person.objects.get(name_parser=spl['person']),
+                                            content=''.join(spl['speeches']).replace(str(':'+spl['person']+':'), ''),
+                                            start_time_stamp=spl['st'],
+                                            end_time_stamp = tb,
+                                            video_id = myfile
+                                )
+                            speech.save()
+                            spl = []
+                            tab = []
+                            tab.append(spe[2])
+                            spl.append({'person': np,'speeches': tab, 'st': toMS(spe[1], 0), 'et': 0})
+                    else:
+                        tab.append(spe[2])
+                        spl.append({'person': np,'speeches': tab, 'st': toMS(spe[1], 0), 'et': 0})
+                else:
+                    tab.append(spe[2])
+                    tb = toMS(spe[1], 1)
 
-			speech = Speech(speaker=Person.objects.get(name_parser=spl[0]['person']),
-	   			content=''.join(spl[0]['speeches']).replace(str(':'+spl[0]['person']+':'), ''),
-	   			start_time_stamp=spl[0]['st'],
-	   			end_time_stamp = tb,
-	   			video_id = myfile
-				)
-			speech.save()
+            speech = Speech(speaker=Person.objects.get(name_parser=spl[0]['person']),
+                content=''.join(spl[0]['speeches']).replace(str(':'+spl[0]['person']+':'), ''),
+                start_time_stamp=spl[0]['st'],
+                end_time_stamp = tb,
+                video_id = myfile
+                )
+            speech.save()
 
 
 def toMS(t, st):
-	spli = t.split(' --> ')
-	t = spli[st].replace(',',":").split(':')
-	seconds = (int(t[0]) * 3600) + (int(t[1]) * 60) + int(t[2]) + (int(t[3]) * float(0.001))
-	return (seconds* 1000)
+    spli = t.split(' --> ')
+    t = spli[st].replace(',',":").split(':')
+    seconds = (int(t[0]) * 3600) + (int(t[1]) * 60) + int(t[2]) + (int(t[3]) * float(0.001))
+    return (seconds* 1000)
 
 
 def getSpeech(request, video_id):
-	data = []
-	speeches = Speech.objects.filter(video_id=str(video_id))
-	print (speeches)
-	for speech in speeches:
-		data.append({'name':speech.speaker.name,
-					 'content': speech.content,
-					 'video_id': speech.video_id,
-					 'start_time_stamp': speech.start_time_stamp,
-					 'end_time_stamp': speech.end_time_stamp
+    data = []
+    speeches = Speech.objects.filter(video_id=str(video_id))
+    print (speeches)
+    for speech in speeches:
+        data.append({'name':speech.speaker.name,
+                     'content': speech.content,
+                     'video_id': speech.video_id,
+                     'start_time_stamp': speech.start_time_stamp,
+                     'end_time_stamp': speech.end_time_stamp
 
-			}
-			)
-	return JsonResponse(data, safe=False)
+                    }
+                   )
+    return JsonResponse(data, safe=False)
+
+
+# SOLR STUFF
+
+
+def exportSpeeches():
+    speeches = Speech.objects.all()
+
+    i=0
+    for speech in speeches:
+        output = [{
+            'id': 'g' + str(speech.id),
+            'speaker_name': speech.speaker.name,
+            'speaker_i': speech.speaker.id,
+            'timestamp_dt': speech.start_time_stamp,
+            'content_t': speech.content,
+            'tip_t': 'govor'
+        }]
+
+        output = json.dumps(output)
+
+        if i % 100 == 0:
+            url = SOLR_URL + '/update?commit=true'
+            r = requests.post(url,
+                              data=output,
+                              headers={'Content-Type': 'application/json'})
+
+
+        else:
+            r = requests.post(SOLR_URL + '/update',
+                              data=output,
+                              headers={'Content-Type': 'application/json'})
+
+        i = i + 1
+    return 1
+
+
+def search(request, words):
+    rows = 50
+    start_page = 0
+    # solr_url = 'http://127.0.0.1:8983/solr/knedl/select?wt=json'
+    solr_url = SOLR_URL + '/select?wt=json'
+
+    q = words.replace('+', ' ')
+
+    solr_params = {
+        'q': 'content_t:' + q.replace('IN', 'AND').replace('!', '+'),
+        'facet': 'true',
+        'facet.range': 'datetime_dt',
+        'facet.range.start': '2014-01-01T00:00:00.000Z',
+        'facet.range.gap': '%2B1MONTHS',
+        'facet.range.end': 'NOW',
+        # 'sort': 'datetime_dt desc',
+        'hl': 'true',
+        'hl.fl': 'content_t',
+        'hl.fragmenter': 'regex',
+        'hl.regex.pattern': '\w[^\.!\?]{1,600}[\.!\?]',
+        'hl.fragsize': '5000',
+        'hl.mergeContiguous': 'false',
+        'hl.snippets': '1',
+        'fq': 'tip_t:govor',
+        'rows': str(rows),
+        'start': str(int(start_page) * rows) if start_page else '0',
+    }
+
+    # print q + 'asd'
+
+    url = solr_url
+    for key in solr_params:
+        url = url + '&' + key + '=' + solr_params[key]
+
+    # print url
+
+    r = requests.get(url).json()
+    return JsonResponse(r)
